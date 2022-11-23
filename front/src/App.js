721c30c1637bc20bcc73cdef675626fb39c42bdd
LHCRYPT <l.hamadouche@laposte.net>
2022-11-23 17:08:31 +0100

    création méthode json

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,32 @@
 import React, { component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import VignetteLocation from './components/VignetteLocation';
 
 function App() {
+  //faire fetch pour chercher json remplacer 6 et 10
+  const locations=[
+    {nom:"location 1 Brest 2.0", image:"photo.jpg"},
+    {nom:"location 2 bord", image:"photo.jpg"},
+    {nom:"location 3 Brest", image:"photo.jpg"}
+  ];
     return (
-        <div className="App">
-          <img src={logo} className="App-logo" alt="logo"/>
-           <header className="App-header">
-            <p>
-          Chez vous, partout et ailleurs
-        </p>
-        
-      </header>
-    </div>
+      
+        <div className="App"> 
+     
+        <div>
+          <img src="logo-kasa.svg" alt="logo Kasa"/>
+           </div>
+           
+           <div class="location"> 
+{locations.map(loc =><VignetteLocation nom={loc.nom}></VignetteLocation>) }
+
+
+  {/*<VignetteLocation nom="location 1 bord"></VignetteLocation>
+  <VignetteLocation nom="location 2 bord"></VignetteLocation>
+   <VignetteLocation nom="location 3 brest"></VignetteLocation>*/}
+
+   </div>
+   </div>
     );
 }
 
---
+++ front/src/components/VignetteLocation.js
@@ -0,0 +1,13 @@
+function VignetteLocation(props) { /*component, la prop ce'est pour envoyer des variables au components*/
+    return (
+      
+        <div className="VignetteLocation"> 
+        <h1>{props.nom}</h1>
+        </div>
+          
+   
+    );
+}
+
+
+export default VignetteLocation;
