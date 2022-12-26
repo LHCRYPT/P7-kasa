1dc96d4973a8e785af7d7221b81183a656e37dcb
LHCRYPT <l.hamadouche@laposte.net>
2022-12-26 23:52:19 +0100

    création routes Home et Page

--- front/src/App.js
+++ front/src/App.js
@@ -1,2 +1,34 @@
+import React, { component, useEffect, useState } from 'react';
+import './App.css';
+import VignetteLocation from './components/VignetteLocation';
+import locations from "./data/logements.json";
+import './components/Home';
+import './components/Page';
 
+function App() {
+ 
+    return (
+      
+        <div className="App"> 
+                <nav>
+                 <a href="/">Home</a>
+                 <a href="/page">Page</a>
+                </nav>
+    
+                <div>
+        <Route path="/page">
+        </Page>
+        </Route>
+        <Route path="/">
+        </Home>
+        </Route>
+
+
+                </div>
+        </div>
+        );
+}
+
+
+export default App;
 
