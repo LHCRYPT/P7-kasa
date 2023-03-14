4526baaed8164fe43afd9bc9a6c0b7af67031a53
LHCRYPT <l.hamadouche@laposte.net>
2023-03-14 18:37:00 +0100

    modifs Banner

--- front/src/components/Banner.js
+++ front/src/components/Banner.js
@@ -1,14 +1,12 @@
-/*import React from "react";*/
 import React, { component, useEffect, useState } from 'react';
 /*import './components/Banner.css';    faire css*/
 
 
-/*function Banner() { mettre la bannière accueil
+function Banner() { //mettre la bannière accueil
     return (
 
   <div className="texteBanner"> <p> Chez vous, partout et ailleurs</p> </div>
         );
 }
 
-export default Banner;*/
-
+export default Banner;
