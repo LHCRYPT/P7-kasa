6811e7d54e340b455d2a777183a825ece9d294fb
LHCRYPT <l.hamadouche@laposte.net>
2023-03-14 18:37:51 +0100

    modifs Footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,11 +1,11 @@
 import React, { component, useEffect, useState } from 'react';
-import Logo from "../logo-kasa.svg";
+//import Logo from "../../public/logo-kasa.svg";
 /*import "../.css"; mettre le lien vers le css*/
 
 function Footer() {
     return (
             <footer className="footer">
-      <img src={Logo} alt="Logo Kasa" ></img>
+      {/* <img src={Logo} alt="Logo Kasa" ></img> */}
          <p> © 2020 Kasa. All rights reserved</p>
     </footer>
 
