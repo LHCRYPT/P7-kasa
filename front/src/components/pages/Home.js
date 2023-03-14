dc787e454a10138f45f29e3f06c59f61bbf1c120
LHCRYPT <l.hamadouche@laposte.net>
2023-03-14 18:40:59 +0100

    modifs Home

--- front/src/components/pages/Home.js
+++ front/src/components/pages/Home.js
@@ -1,22 +1,21 @@
 import React, { component, useEffect, useState } from 'react';
 
-/*import Banner from "../components/Banner";*/
+import Header from "../Header";
 
 /*import PageLogement from "./components/pages/PageLogement";*/
 
 
-/*import Card from "../../components/Card";*/
-/*import { Link } from "react-router-dom";*/
-
-/*import "../../css"; A faire*/
 
 
 function Home() {
     
     return (
+    <>
+    <Header/>
     <h1>
         Home
     </h1>
+    </>
     );
   }
   
