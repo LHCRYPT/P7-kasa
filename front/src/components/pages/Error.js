959a280459b0e5644ba0eaa424a63f49a8431e6a
LHCRYPT <l.hamadouche@laposte.net>
2023-03-19 03:23:00 +0100

    ajout Accueil et About et logo import

--- front/src/components/pages/Error.js
+++ front/src/components/pages/Error.js
@@ -1,12 +1,28 @@
 import React, { component, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
+import Logo from "../../logo-kasa.svg";
 import Header from "../Header";
 /*import './components/pages/Error.css';    faire css*/
 
 function Error() {
     return (
-      
+      <> 
+       <header className="header">
+<img className="logoHeader" src={Logo} alt="Logo Kasa" ></img>
+  
+     
+  <nav className="headerLink">
+     <div>
+       <Link to={`/`}><p className="Accueil">Accueil</p></Link>
+     </div>
+     <div>
+       <Link to={`/About`}><p className="APropos">A Propos</p></Link>
+     </div>
+   </nav>
+    </header> 
+
+
       <div className="erreur">
         <h1 className="nombre">404</h1>
         <p className="texte"> Oups ! La page que vous demandez n'existe pas. </p>
@@ -14,7 +30,7 @@ function Error() {
           Retourner sur la page d'accueil
         </Link>
       </div>
-    );
+      </>   );
   }
   
   export default Error;
