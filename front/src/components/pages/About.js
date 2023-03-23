424b9edfdbfc467a871c488d5e4e1f930a4e151d
LHCRYPT <l.hamadouche@laposte.net>
2023-03-24 00:16:35 +0100

    ajout vectors haut et bas

--- front/src/components/pages/About.js
+++ front/src/components/pages/About.js
@@ -5,7 +5,10 @@ import Logo from "../../logo-kasa.svg";
 import imageBannerBleue from "../../imageBannerBleue.jpg";
 import Footer from "../Footer";
 import styles from "./About.css";
+import VectorHaut from "../../VectorHaut.png";
+import VectorBas from "../../VectorBas.png";
 // import Collapse from "./Collapse";
+
 function About() {
     return (
         <> 
@@ -34,7 +37,33 @@ function About() {
     <p className="Sécurité"> Sécurité</p>  */}
 
  {/* <Collapse/> enlever le commentaire quand fichier ciollapse est fait  */}
-    
+ <section className='section-element'>
+            
+            <div className='collapse-element'>
+                <Collapse
+                title= "Fiabilité"
+                content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+                />
+            </div>
+            <div className='collapse-element'>
+                <Collapse 
+                title= "Respect"
+                content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.."
+                />
+            </div>
+            <div className='collapse-element'>
+                <Collapse 
+                title= "Service"
+                content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+                />
+            </div>
+            <div className='collapse-element'>
+                <Collapse 
+                title= "Sécurité"
+                content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                />
+            </div>
+        </section>
     <Footer/>
    
     </>  );
