936849a68f06d57b9ac4c9bdee9fd0b4a32b83f3
LHCRYPT <l.hamadouche@laposte.net>
2023-03-26 17:19:48 +0200

    modifs nom de chaque icône flêche

--- front/src/components/Collapse.js
+++ front/src/components/Collapse.js
@@ -1,6 +1,6 @@
 import React, { component, useEffect, useState } from 'react'; /* importation de Hook useState depuis React. Il me permet d’utiliser un état local dans une fonction composant*/
-import VectorHaut from "../VectorHaut.png";
-import VectorBas from "../VectorBas.png";
+import isOpen from "../isOpen.png";
+import setIsOpen from "../setIsOpen.png";
 import styles from "./Collapse.css";
 
 /*ici ce trouve l'encadrement de fiabilité et l'icône flèche*/
@@ -13,13 +13,13 @@ import styles from "./Collapse.css";
 //   );
 // }
 
-const Collapse = (props) => {
+const Collapse = (props) => {  
     //collapse fermé
-    const [open, setOpen] = useState(false); /*déclaration d'une nouvelle variable d’état en appelant le Hook useState,renvoie une paire de valeurs que nous pouvons nommer à notre guise, useState déclare une « variable d’état » (appelé open), Le second élément renvoyé est une fonction. Elle nous permet de modifier la variable open, appelée setOpen.*/
+    const [isOpen, setIsOpen] = useState(false); /*déclaration d'une nouvelle variable d’état en appelant le Hook useState,renvoie une paire de valeurs que nous pouvons nommer à notre guise, useState déclare une « variable d’état » (appelé open), Le second élément renvoyé est une fonction. Elle nous permet de modifier la variable open, appelée setOpen.*/
   
     //  collapse ouvert (toggle)
     const toggle = () => {
-      setOpen(!open);
+      setIsOpen(!isOpen);
     };
   
     return (
@@ -44,6 +44,36 @@ const Collapse = (props) => {
     );
   };
 export default Collapse;
+
+/* function Cart({ cart, updateCart }) {
+    const monsteraPrice = 8
+    const [isOpen, setIsOpen] = useState(true)
+
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(false)}
+            >
+                Fermer
+            </button>
+            <h2>Panier</h2>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>Vider le panier</button>
+        </div>
+    ) : (
+        <div className='lmj-cart-closed'>
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(true)}
+            >
+                Ouvrir le Panier
+            </button>
+        </div>
+    )
+}
+
+export default Cart */
 
 /*  <i className="fa-solid fa-chevron-up"></i>*/
 /*  <i className="fa-solid fa-chevron-down"></i> */
