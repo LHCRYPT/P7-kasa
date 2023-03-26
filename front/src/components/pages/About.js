8c2d5a4ba884434f13cfc4c851666a0149fd3cf2
LHCRYPT <l.hamadouche@laposte.net>
2023-03-26 18:16:08 +0200

    modifs texte de Service dans about

--- front/src/components/pages/About.js
+++ front/src/components/pages/About.js
@@ -32,7 +32,7 @@ function About() {
      />
     <Collapse titre= "Respect" text=" La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. " 
      />
-     <Collapse titre= "Service" text=" Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes. " 
+     <Collapse titre= "Service" text=" Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question. " 
      />
      <Collapse titre= "Sécurité" text=" Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes. " 
      />
