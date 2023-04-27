6f2e00622a4640a4c0ab684e8f86b20c62b8d8dd
LHCRYPT <l.hamadouche@laposte.net>
2023-04-27 03:00:57 +0200

    suppression et modfifs import dans Header.js

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,8 +1,9 @@
-import React, { component, useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
-import Logo from "../logo-kasa.svg";
+import "./Header.css";
+import { Link,NavLink } from "react-router-dom";
+import logo from "../assets/logo.svg";
+/* Link remplace la balise a */
 
-import styles from "./Header.css";
+
 
 
 function Header() {
