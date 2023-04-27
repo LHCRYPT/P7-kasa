96bd5d7e8c9d6ae526b3206d4e2181e5f56979ca
LHCRYPT <l.hamadouche@laposte.net>
2023-04-27 02:00:57 +0200

    ajout chemin import logo dans error.js

--- front/src/pages/Error.js
+++ front/src/pages/Error.js
@@ -1,5 +1,6 @@
 import "./Error.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import React, { component, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
