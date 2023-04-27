297792cec86edde3b784fbe2aa824a2a66f62031
LHCRYPT <l.hamadouche@laposte.net>
2023-04-27 02:42:50 +0200

    modif className et on click dans collapse.js

--- front/src/components/Collapse.js
+++ front/src/components/Collapse.js
@@ -3,38 +3,21 @@ import { useState } from 'react'
 import openImg from '../assets/icon/open.png'
 import closeImg from '../assets/icon/close.png'
 
-
-
-const Collapse = (props) => {  
-    //collapse fermé
-    const [isOpen, setIsOpen] = useState(false); /*déclaration d'une nouvelle variable d’état en appelant le Hook useState,renvoie une paire de valeurs que nous pouvons nommer à notre guise, useState déclare une « variable d’état » (appelé open), Le second élément renvoyé est une fonction. Elle nous permet de modifier la variable open, appelée setOpen.*/
-  
-    //  collapse ouvert (toggle)
-    const toggle = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <div className="collapse-element">
-        
-        <div className="button-container" onClick={toggle}> {/* toggle collapse Cliquez sur les boutons pour afficher/masquer un autre élément via les changements de classe */}
-          <div className="collapse-titre">
-            
-            {props.titre}
-          </div>
-          <div className="collapse-button">
-          {isOpen ? ( /* if open ne pas mettre if,c'est if ternaire car il y a ? et :*/
-            <img className="vector" src={flecheHaut } /> /* si flêche vers le haut */
-          ) : ( /* else  */
-            
-            <img className="vector" src={flecheBas} />// si flêche vers le bas
-          )}</div>
-        </div>
-       
-        {isOpen && <div className="collapse-element"> {props.text} </div>} {/* si flêche vers le haut, && = if ternaire avec condition */}
+export default function Collapse({titre,texte,liste}) {
+  const [deplier, setDeplier] = useState(false);
+  function changer(){
+      setDeplier(!deplier);
+  }
+  return (
+      <div className="collapse">
+      <div className="banner-collapse" onClick={changer}>
+          {titre}
+          {deplier 
+          ? <img src={closeImg} alt="Fermer" /> 
+          : <img src={openImg} alt="Ouvrir"  />}
       </div>
-    );
-  };
-export default Collapse;
-
-
+      {/* si deplier est true alors jaffiche */}
+      {deplier && (<div className="banner-texte">{texte}</div>)}
+  </div>
+  );
+}
