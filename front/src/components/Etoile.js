0f3e170d2ac86fb87b30b68f08c003fde746b3e1
LHCRYPT <l.hamadouche@laposte.net>
2023-04-27 02:48:28 +0200

    modif de rating dans etoile.js

--- front/src/components/Etoile.js
+++ front/src/components/Etoile.js
@@ -2,28 +2,17 @@ import etoileGrise from '../assets/icon/etoile-grise.png'
 import etoileRose from '../assets/icon/etoile-rose.png'
 
 
-
-const Rating = ({ rating }) => {
+export default function Etoiles({rating}) {
     const etoiles = [1, 2, 3, 4, 5];
     
       return (
-        <div className="toile">
-          {etoiles.map((etoile) =>
-            rating >= etoile ? (
-              <img
-                key={etoile.toString()}
-                className="etoileIcone"
-                src={etoileRouge} alt="étoile rouge"
-              />
-            ) : (
-              <img
-                key={etoile.toString()}
-                className="etoileIcones"
-                src={etoileBlanche} alt="étoile blanche"
-              />
-            )
-          )}
-        </div>
-      )
+        <>
+        {etoiles.map((etoile) =>
+            rating >= etoile 
+            ? <img src={etoileRose} alt="étoile rose" />        
+            :<img src={etoileGrise} alt="étoile grise" /> 
+        )}
+        </>
+    );
   }
-  export default Rating;
+      
